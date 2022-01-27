d0a5be1f70fbe7172dd9332bce1e5a062ffe8428
vinniciuscg <vinicius.mvcg5991@gmail.com>
2022-01-27 20:34:45 -0300

    ServerList finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Discord Clone</h1>
-    </div>
+    <>
+      <Layout />
+      
+      <GlobalStyles />
+    </>
   );
 }
 
